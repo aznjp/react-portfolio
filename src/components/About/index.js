1a02ab49af69545bb6af4494cd5ab82640eb562b
aznjp <jpark103193@gmail.com>
2021-01-10 01:38:59 -0500

    added in About me component and added in stylings to nav-bar and about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
-const About = () => {
+import Typewriter from "typewriter-effect";
+import profileImage from "../../assets/John.png"
+
+function About() {
     return (
-        <div>
-            About
+    <main className="bg-about bg-cover pt-20 pb-20">
+      <div className="mx-auto w-10/12 text-center text-white">
+        <div className="text-4xl pb-4">
+          <h2>Hi, my name is John and I am a...</h2>
+          <Typewriter
+              options={{
+                strings: ["Web Developer", "Researcher", "Student"],
+                autoStart: true,
+                loop: true,
+                delay: 150
+              }}
+          /> 
         </div>
+
+        <section className="text-center">
+          <h2 className="mb-6 text-6xl">About Me</h2>
+          <img
+            src={profileImage}
+            className="object-scale-down mb-5 inline-block rounded-full items-center justify-center..."
+            alt="John Park"
+          />
+          <div className="mb-16 text-2xl">
+            <p>
+                Hello, my name is John and I am a University of Central Florida graduate with a bachelor's degree in chemistry currently working as a research assistant at Sanofi Pasteur Vaxdesign. Currently I am looking forward to learning the next edge in technology
+                which is why I chose to dive into the world of coding and development. Right now I am still a fresh young tadpole diving into this vast ocean of knowledge that many have trekked over the last couple of decades, but I hope that in the
+                future I could become a whole new person. One of many who can become one of the pioneers of this technological frontier.
+            </p>
+          </div>
+        </section>
+        
+      </div>
+    </main>
     )
 }
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// import logo from "../../logo.svg";
 import "../../App.css";
 import "../../index.css";
 
@@ -14,20 +15,22 @@ function Navigation(props) {
     } = props;
 
   return (
-      <nav>
-        <ul>
-          {tabs.map((tabs) => (
-            <li className= {`${currentTab === tabs && 'navActive'}`} key={tabs} >
-            <span
-                onClick={() => {
-                setCurrentTab(tabs)
-                }}
-            >
-            {tabs}
-            </span>
-            </li>
-          ))}
-        </ul>
+      <nav className ="flex items-center justify-center lg:justify-between flex-wrap w-full h-36 text-white text-4xl">
+        <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block">
+          <ul className = "flex justify-center lg:justify-end flex-1 items-center text-2xl xl:text-2xl">
+            {tabs.map((tabs) => (
+              <li className= {`pl-4 pr-4 ${currentTab === tabs && 'navActive'}`} key={tabs} >
+              <span
+                  onClick={() => {
+                  setCurrentTab(tabs)
+                  }}
+              >
+              {tabs}
+              </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </nav>
   );
 }
