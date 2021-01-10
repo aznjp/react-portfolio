961660b482d013d6192017f22845e8e64fc6c6c7
aznjp <jpark103193@gmail.com>
2021-01-10 02:49:30 -0500

    updated navbar to display highlights for selected tab

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 // import logo from "../../logo.svg";
 import "../../App.css";
@@ -14,12 +14,17 @@ function Navigation(props) {
         setCurrentTab
     } = props;
 
+  /* Added in hook to allow for document.title to be visible to user once they click on the set tab */
+  useEffect(() => {
+      document.title = currentTab
+  }, [currentTab]);
+
   return (
       <nav className ="flex items-center justify-center lg:justify-between flex-wrap w-full h-36 text-white text-4xl">
         <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block">
           <ul className = "flex justify-center lg:justify-end flex-1 items-center text-2xl xl:text-2xl">
             {tabs.map((tabs) => (
-              <li className= {`pl-4 pr-4 ${currentTab === tabs && 'navActive'}`} key={tabs} >
+              <li className= {`pl-4 pr-4 cursor-pointer ${currentTab === tabs && 'navActive text-purple-300'}`} key={tabs} >
               <span
                   onClick={() => {
                   setCurrentTab(tabs)
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Portfolio = () => {
+function Portfolio () {
     return (
         <div>
             Portfolio
